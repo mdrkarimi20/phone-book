dd388899f280d8437610b91d184aebabddf69057
mdrkarimi20 <mdrkarimi20@gmail.com>
2020-07-23 16:50:04 +0430

    Updates

---
+++ precache-manifest.9fac7568a47006bc75c780422c7b9413.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce075b2d608470072f48933e688aa7c5",
+    "url": "/phonebook/index.html"
+  },
+  {
+    "revision": "91e113bd71f717617283",
+    "url": "/phonebook/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "54ea0cdd7df13d240bc7",
+    "url": "/phonebook/static/css/main.eaf20724.chunk.css"
+  },
+  {
+    "revision": "91e113bd71f717617283",
+    "url": "/phonebook/static/js/2.a85b3795.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/phonebook/static/js/2.a85b3795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54ea0cdd7df13d240bc7",
+    "url": "/phonebook/static/js/main.441996f6.chunk.js"
+  },
+  {
+    "revision": "72f193dfaf21fd8fe8ce",
+    "url": "/phonebook/static/js/runtime-main.0c970a85.js"
+  }
+]);
